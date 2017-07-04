a221cd70d9f3e99452448a131f0a185b8da7e096
lanyotech <woweiwokuang@gmail.com>
2017-07-04 19:26:39 +0800

    小区租售

---
+++ Application/Admin/Controller/SaleController.class.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/7/4
+ * Time: 14:19
+ */
+
+namespace Admin\Controller;
+
+
+class SaleController extends AdminController
+{
+    public function index(){
+
+
+        $this->display("index");
+    }
+
+
+
+}
